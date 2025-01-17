d278e3a169488a66059bd7034433bcb91526e7d0
Kimi730 <1213284@student.roc-nijmegen.nl>
2025-01-17 13:39:02 +0100

    .

--- src/masterMind30/FunctieMastermind2.java
+++ src/masterMind30/FunctieMastermind2.java
@@ -5,103 +5,89 @@ import java.util.Scanner;
 
 public class FunctieMastermind2 {
 
+	String[] secretCodes = new String[4];
+
+	String[] playerGuess = new String[4];
+
+	String allCorrect;
+
+	String allCodes;
+
 	public String[] generateCode() {
 
 		String[] pickColors = { "R", "Y", "G", "B", "P", "O" };
 
 		Random rand = new Random();
 
-		String[] secretCodes = new String[4];
-
 		for (int x = 0; x < secretCodes.length; x++) {
 			secretCodes[x] = pickColors[rand.nextInt(6)];
-
-			return secretCodes;
 		}
+
+		allCodes = secretCodes[0] + secretCodes[1] + secretCodes[2] + secretCodes[3];
+
+		return secretCodes;
 	}
 
-	public int round() {
+	public void userInput() {
 
-		for (int roundTen = 1; roundTen <= 10; roundTen++) {
+		Scanner sc = new Scanner(System.in);
 
-			System.out.println("");
-
-			System.out.println("Round " + roundTen);
-
-			if (roundTen >= 10) {
-				System.out.println("You lost ! ! !");
-
+		for (int j = 0; j < playerGuess.length; j++) {
+			playerGuess[j] = sc.next();
+			allCorrect = playerGuess[0] + playerGuess[1] + playerGuess[2] + playerGuess[3];
 			}
 		}
-	}
 
-	public String userInput() {
-		Scanner sc = new Scanner(System.in);
-		String[] playerGuess = new String[4];
-
-		for (int j = 0; j < playerGuess.length; j++)
-
-			playerGuess[j] = sc.next();
-		sc.close();
-	}
-
-	public String[] howtoWin() {
-
-		String allCorrect = playerGuess[0] + playerGuess[1] + playerGuess[2] + playerGuess[3];
-
-		String allCodes = secretCodes[0] + secretCodes[1] + secretCodes[2] + secretCodes[3];
+	public void howtoWin() {
 
 		if (allCodes.equals(allCorrect)) {
 
 			System.out.println("");
 			System.out.println("Gongratulations ! ! !");
-			break;
+
 		}
-	}
-
-	public String[] colortoCheck() {
-
-		String[] checkColors = { "X", "W" };
-
-		return checkColors;
 	}
 
 	public String[] ifElse() {
 
+		String[] checkColors = { "X", "W" };
+
 		if (playerGuess[0].equals(secretCodes[0])) {
-			System.out.print(checkColors[0]);
+			System.out.print(checkColors[0] + " ");
 		} else if (playerGuess[0].equals(secretCodes[1]) || playerGuess[0].equals(secretCodes[2])
 				|| playerGuess[0].equals(secretCodes[3])) {
-			System.out.print(checkColors[1]);
+			System.out.print(checkColors[1] + " ");
 		} else {
-			System.out.print(" ");
+			System.out.print("  ");
 		}
 
 		if (playerGuess[1].equals(secretCodes[1])) {
-			System.out.print(" " + checkColors[0]);
+			System.out.print(checkColors[0] + " ");
 		} else if (playerGuess[1].equals(secretCodes[0]) || playerGuess[1].equals(secretCodes[2])
 				|| playerGuess[1].equals(secretCodes[3])) {
-			System.out.print(" " + checkColors[1]);
+			System.out.print(checkColors[1] + " ");
 		} else {
-			System.out.print(" ");
+			System.out.print("  ");
 		}
 
 		if (playerGuess[2].equals(secretCodes[2])) {
-			System.out.print(" " + checkColors[0]);
+			System.out.print(checkColors[0] + " ");
 		} else if (playerGuess[2].equals(secretCodes[0]) || playerGuess[2].equals(secretCodes[1])
 				|| playerGuess[2].equals(secretCodes[3])) {
-			System.out.print(" " + checkColors[1]);
+			System.out.print(checkColors[1] + " ");
 		} else {
-			System.out.print(" ");
+			System.out.print("  ");
 		}
 
 		if (playerGuess[3].equals(secretCodes[3])) {
-			System.out.print(" " + checkColors[0]);
+			System.out.print(checkColors[0] + " ");
 		} else if (playerGuess[3].equals(secretCodes[0]) || playerGuess[3].equals(secretCodes[1])
 				|| playerGuess[3].equals(secretCodes[2])) {
-			System.out.print(" " + checkColors[1]);
+			System.out.print(checkColors[1] + " ");
 		} else {
-			System.out.print(" ");
+			System.out.print("  ");
 		}
+
+		return checkColors;
 	}
 }
--- src/masterMind30/Mastermind2.java
+++ src/masterMind30/Mastermind2.java
@@ -1,33 +1,18 @@
 package masterMind30;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Mastermind2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("Welcome to Mastermind,Pick four colors,You have 10 chances");
 		System.out.println(
 				"If you get a X, it means you have it at the right place, if its W, it means it is at the wrong place and none of both means it doesn't exist.");
-		System.out.println("Turn on caps lock, this will be handy, type in one line with spaces between each letter");
 		System.out.println("Pickable colors: R,Y,G,B,P,O");
-		
+		System.out.println("Turn on caps lock, this will be handy, type in one line with spaces between each letter");
+
 		FunctieMastermind2 functie = new FunctieMastermind2();
-		
-		String[] pickColors = { "R", "Y", "G", "B", "P", "O" };
 
-		String[] checkColors = { "X", "W" };
-
-		Random rand = new Random();
-
-		String[] secretCodes = new String[4];
-		for (int x = 0; x < secretCodes.length; x++) {
-			secretCodes[x] = pickColors[rand.nextInt(6)];
-		}
-
-		Scanner sc = new Scanner(System.in);
+		functie.generateCode();
 
 		for (int roundTen = 1; roundTen <= 10; roundTen++) {
 
@@ -35,59 +20,21 @@ public class Mastermind2 {
 
 			System.out.println("Round " + roundTen);
 
-			String[] playerGuess = new String[4];
+			System.out.println("Pickable colors: R,Y,G,B,P,O");
 
-			for (int j = 0; j < playerGuess.length; j++)
+			functie.userInput();
 
-				playerGuess[j] = sc.next();
+			functie.ifElse();
 
-			if (playerGuess[0].equals(secretCodes[0])) {
-				System.out.print(checkColors[0]);
-			} else if (playerGuess[0].equals(secretCodes[1]) || playerGuess[0].equals(secretCodes[2])
-					|| playerGuess[0].equals(secretCodes[3])) {
-				System.out.print(checkColors[1]);
-			} else {
-				System.out.print("  ");
+			functie.howtoWin();
+			
+			if (roundTen == 9) {
+				System.out.println("Last chance!");
 			}
-
-			if (playerGuess[1].equals(secretCodes[1])) {
-				System.out.print(" " + checkColors[0]);
-			} else if (playerGuess[1].equals(secretCodes[0]) || playerGuess[1].equals(secretCodes[2])
-					|| playerGuess[1].equals(secretCodes[3])) {
-				System.out.print(" " + checkColors[1]);
-			} else {
-				System.out.print("  ");
-			}
-
-			if (playerGuess[2].equals(secretCodes[2])) {
-				System.out.print(" " + checkColors[0]);
-			} else if (playerGuess[2].equals(secretCodes[0]) || playerGuess[2].equals(secretCodes[1])
-					|| playerGuess[2].equals(secretCodes[3])) {
-				System.out.print(" " + checkColors[1]);
-			} else {
-				System.out.print("  ");
-			}
-
-			if (playerGuess[3].equals(secretCodes[3])) {
-				System.out.print(" " + checkColors[0]);
-			} else if (playerGuess[3].equals(secretCodes[0]) || playerGuess[3].equals(secretCodes[1])
-					|| playerGuess[3].equals(secretCodes[2])) {
-				System.out.print(" " + checkColors[1]);
-			} else {
-				System.out.print("  ");
-			}
-
-			String allCorrect = playerGuess[0] + playerGuess[1] + playerGuess[2] + playerGuess[3];
-			String allCodes = secretCodes[0] + secretCodes[1] + secretCodes[2] + secretCodes[3];
-			if (allCodes.equals(allCorrect)) {
-				System.out.println("");
-				System.out.println("Gongratulations ! ! !");
-				break;
-			}
-
+			
 			if (roundTen >= 10) {
-				System.out.println("You lost ! ! !");
-				sc.close();
+				System.out.println("You lost ! ! !" + " The code was: " + functie.allCodes);
+				break;
 			}
 		}
 	}
